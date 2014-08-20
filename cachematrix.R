f99ca4b9bf451d80ebe046f7f227f0098fdffde6
Peter Sabaini <peter@sabaini.at>
2014-08-20 21:42:46 +0200

    skel

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        cache <- NULL
+        set <- function(y) {
+                x <<- y
+                cache <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) cache <<- inverse
+        getinverse <- function() cache
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cache <- x$getinverse()
+        if(!is.null(cache)) {
+                message("getting cached data")
+                return(cache)
+        }
+        data <- x$get()
+        cache <- solve(data, ...)
+        x$setinverse(cache)
+        cache
 }
+
+
+
